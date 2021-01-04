5196c8568658a75ab3384f29638367fcea54afaf
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-04 17:04:31 -0600

    Detect url with http and https and format accordingly

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,16 @@
 class LinksController < ApplicationController
     
+
+    post '/link' do 
+        params.to_s
+        #{"name"=>"TikTok", "url"=>"https://www.tiktok.com/@yehudabortz"}
+        if !logged_in? || params.values.any?("")
+            redirect '/dashboard'
+        else
+            @user = current_user
+            @user.links << @link = Link.create(params)
+            redirect '/dashboard'
+        end
+    end
+
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,14 @@
 class Link < ActiveRecord::Base
     belongs_to :user
+
+    def format_url 
+        http = "http://"
+        https = "https://"
+        if !self.url.downcase.include? http || https
+            http + self.url
+        else
+            self.url
+        end
+    end
+
 end
