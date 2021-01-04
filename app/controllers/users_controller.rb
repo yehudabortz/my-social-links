e19ba4a337ddd11f7bbb6b573bcdda36dd980066
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-04 12:06:48 -0600

    Users Controller Routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
+    
+    get '/signup' do 
 
+    end
+
+    post '/signup' do 
+
+    end
+
+    get '/login' do 
+
+    end
+
+    post '/login' do 
+
+    end
+
+    get '/dashboard' do 
+
+    end
+
+    get '/logout' do
+
+    end
 end
