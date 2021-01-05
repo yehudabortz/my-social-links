501d8495e6fd79e7d3575fd46986cd10fdecc777
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-04 21:20:13 -0600

    Minimize http requests by comparing if the urls are different from original and updated link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,7 +3,6 @@ class LinksController < ApplicationController
 
 
     post '/link' do 
-    
         if  params.values.any?("")
             redirect '/dashboard'
         else
@@ -18,5 +17,24 @@ class LinksController < ApplicationController
             redirect '/dashboard'
         end
     end
+    
+    patch '/link' do
+        params[:original_link][:id].each_with_index do |id, index|
+            @link = Link.find(id)
+            unless @link.name == params[:link][:name][index]
+                @link.name = params[:link][:name][index]
+                @link.save
+            end
+            unless @link.url == params[:link][:url][index]
+                @link.url = params[:link][:url][index]
+                unless @link.valid_url?
+                    @link.save
+                end
+            end
+        end
+        redirect '/dashboard'
+    end
+    
 
+    
 end
