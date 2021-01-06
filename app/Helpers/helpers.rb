fd480c86df8d2d6f4b0fc03e65a84d18c8172b5b
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-05 20:41:08 -0600

    Modified Password Regex to only allow Minimum 8 chars, min 1 number, 1 captial, 1 lowercase, and no special characters

--- app/Helpers/helpers.rb
+++ app/Helpers/helpers.rb
@@ -13,7 +13,7 @@ module Helpers
     end
     
     def validate_signup_details
-        params[:username].match(/^(?=[a-zA-Z0-9._]{4,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/) && params[:email].match(/[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/) && params[:password].match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+        params[:username].match(/^(?=[a-zA-Z0-9._]{1,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/) && params[:email].match(/[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/) && params[:password].match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
     end
 
     def user_exists?
