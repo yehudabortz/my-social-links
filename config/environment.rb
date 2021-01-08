ecc43bbf2c95a98a2083399d34cb3a3c2dd80de7
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-07 18:07:15 -0600

    Local Sqlite Db working in development group

--- config/environment.rb
+++ config/environment.rb
@@ -1,32 +1,29 @@
-##original
-# ENV['SINATRA_ENV'] ||= "development"
+#original
+ENV['SINATRA_ENV'] ||= "development"
 
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 
-# ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
-
+require 'timeout'
+require 'open-uri'
+require_all 'app'
+require 'sinatra'
 
 configure :development do
-    ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-  )
+    ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
+
   end
   
-  configure :production do
-    db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://localhost/mydb')
-  
+configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ 
     ActiveRecord::Base.establish_connection(
-      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
       :host     => db.host,
       :username => db.user,
       :password => db.password,
       :database => db.path[1..-1],
       :encoding => 'utf8'
-    )
-  end
-require 'timeout'
-require 'open-uri'
-require_all 'app'
+      )
+ end
