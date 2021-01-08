72fc931dea1c84dd77971c34649854cce8368622
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-07 20:37:48 -0600

    Undid change, login now redirects to '/login' upon failed attempt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
             redirect '/dashboard'
         else
             flash[:message] = "Unable to log in. Try Again!"
-            redirect '/'
+            redirect '/login'
         end
     end
     
