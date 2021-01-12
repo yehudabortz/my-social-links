4d7ee94a4f72cf355fcc645f0f5af37e854cf7fa
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-12 09:24:22 -0600

    Remove redundent method for validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,4 @@ class User < ActiveRecord::Base
     
     has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
     has_many :following, through: :following_relationships, source: :following
-
-
-    def valid_username?
-        if self.username.match(/^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/)
-            true
-        else
-            false
-        end
-    end
-
 end
